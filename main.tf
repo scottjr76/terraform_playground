cea7977785aeb8a275cea72f2dc65957c77c245b
Scott Anderson <scottanderson@northwesternmutual.com>
2019-02-14 15:01:32 -0600

    Minor format changes.

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
   user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
 
   tags {
       Name = "terraform-example"
