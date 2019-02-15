744d18bf1ff92332f0b0014f0039ade1b70ef36f
Scott Anderson <scottanderson@northwesternmutual.com>
2019-02-15 13:03:32 -0600

    Added ELB.

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ variable "server_port" {
 data "aws_availability_zones" "all" {
 }
 
+output "elb_dns_name" {
+  value = "${aws_elb.example.name}"
+}
+
 resource "aws_launch_configuration" "example" {
   image_id = "ami-40d28157"
   instance_type = "t2.micro"
@@ -45,6 +49,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
 
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -52,5 +59,42 @@ resource "aws_autoscaling_group" "example" {
     key                 = "Name"
     value               = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
+  }
+}
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
