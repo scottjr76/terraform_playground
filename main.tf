ac603776c3665a08385f3ff25f54ef7d50f981f8
Scott Anderson <scottanderson@northwesternmutual.com>
2019-02-15 10:36:57 -0600

    Added ASG.

--- main.tf
+++ main.tf
@@ -9,14 +9,13 @@ variable "server_port" {
   default = 8080
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+data "aws_availability_zones" "all" {
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -24,8 +23,8 @@ resource "aws_instance" "example" {
     nohup busybox httpd -f -p "${var.server_port}" &
     EOF
 
-  tags {
-      Name = "terraform-example"
+lifecycle {
+    create_before_destroy = true
   }
 }
 resource "aws_security_group" "instance" {
@@ -36,5 +35,22 @@ resource "aws_security_group" "instance" {
       to_port     = "${var.server_port}"
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
