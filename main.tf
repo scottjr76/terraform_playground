ad9e0cb8dce2e9df845b78ab138f2c6ecd3ec67a
Scott Anderson <scottanderson@northwesternmutual.com>
2019-02-15 09:16:24 -0600

    Added security group to EC2 instance.

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
     #!/bin/bash
